a94f54dd4afdbb6a1e64a3096af1934b15c9aef9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-24 18:14:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :coupons, only: [:index, :new, :create]
+  get '/coupon/:id', to: 'coupon#show', as: 'coupon'
 end
